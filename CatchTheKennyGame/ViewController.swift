72a9af7e43b4b3ee2d9fe4cf73cd7c78076f1500
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-05 20:54:11 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Mustafa Adnan Tatlıcı on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
